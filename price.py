acc69e5f1ad7c6e4ffa89fb0924546caf57cbc02
hemavarshiniloya <112843810+hemavarshiniloya@users.noreply.github.com>
2022-09-21 19:55:51 +0530

    Update price.py

--- price.py
+++ price.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pickel
 pickel_in=open('diamond.pkl','rb')
 mond=pickel.load(pickel_in)
-a=st.number_input('carat')
-b=st.number_input('cut')
-c=st.number_input('color')
-d=st.number_input('depth')
-e=st.number_input('table')
+a=st.number_input('enter carat')
+b=st.number_input('enter cut')
+c=st.number_input('enter color')
+d=st.number_input('enter depth')
+result=0
 if st.button('PREDICT'):
+  result=clf.predict([[a,b,c,d]]).squeeze()
   st.success(f'DIAMOND PRICE IS {PRICE})
